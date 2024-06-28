7a22f64bcd7d148e999fa1339420f77f90b83fab
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-06-27 23:59:32 -0600

    starting repository java configuration properties

---
+++ src/main/java/com/java_configuration_properties/JavaConfigurationPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.java_configuration_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConfigurationPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConfigurationPropertiesApplication.class, args);
+	}
+
+}
