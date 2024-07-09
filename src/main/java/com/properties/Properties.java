305d987ede5ccc6f5d2e1ba99659ebfc3e5c2607
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-07-08 19:22:19 -0600

    add tags Configuracion and ConfigurationProperties

--- src/main/java/com/properties/Properties.java
+++ src/main/java/com/properties/Properties.java
@@ -1,8 +1,12 @@
 package com.properties;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Data
+@Configuration
+@ConfigurationProperties(prefix = "properties")
 public class Properties {
 
   private String string;
