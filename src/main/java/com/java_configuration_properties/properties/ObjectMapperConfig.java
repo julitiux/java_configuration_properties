d2b3098779e78e2e1c9fed6e792f40332e7ec248
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-11-30 01:27:11 -0600

    implement bean objectMapper

--- src/main/java/com/java_configuration_properties/properties/ObjectMapperConfig.java
+++ src/main/java/com/java_configuration_properties/properties/ObjectMapperConfig.java
@@ -1,4 +1,15 @@
 package com.java_configuration_properties.properties;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+
 public class ObjectMapperConfig {
+
+  @Bean
+  public ObjectMapper objectMapper(){
+    return new ObjectMapper()
+      .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  }
+
 }
