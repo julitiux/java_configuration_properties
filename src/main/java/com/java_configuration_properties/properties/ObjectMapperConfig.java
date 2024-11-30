cd0d253bb79234d3556ee4fd666d80c77933eb51
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-11-30 01:27:48 -0600

    add tag Configuration

--- src/main/java/com/java_configuration_properties/properties/ObjectMapperConfig.java
+++ src/main/java/com/java_configuration_properties/properties/ObjectMapperConfig.java
@@ -3,7 +3,9 @@ package com.java_configuration_properties.properties;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ObjectMapperConfig {
 
   @Bean
