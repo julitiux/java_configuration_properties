7347a7f6e03eee9e71ebcfe6c3ce3fa44ed1b719
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-12-01 23:28:54 -0600

    modify the property comunication object to Map and Lisy communication

--- src/main/java/com/java_configuration_properties/properties/Notification.java
+++ src/main/java/com/java_configuration_properties/properties/Notification.java
@@ -3,8 +3,11 @@ package com.java_configuration_properties.properties;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.List;
+import java.util.Map;
+
 @Data
 @ToString
 public class Notification {
-  private Comunication comunication;
+  private Map<String, List<Comm>> communication;
 }
